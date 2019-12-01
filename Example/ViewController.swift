a6f031e71cba0da5eb7beac83653b0300b3e99a3
fmorales <fmorales@science37.com>
2019-11-30 17:45:23 -0800

    init commit

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Frank Morales on 11/30/19.
+//  Copyright © 2019 Frank Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
